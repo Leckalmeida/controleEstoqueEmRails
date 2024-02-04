efbad042dea6b2abdaa385863edb9327b590c35f
Leckalmeida <leckalmeida@gmail.com>
2024-02-04 14:14:22 -0300

    implementacao de features

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,8 @@
 class Produto < ApplicationRecord
+
+    belongs_to :departamento, optional: true
+
+    validates :preco, presence: true
+    validates :nome, length: { minimum: 4 }
+    validates :descricao, length: { minimum: 10, maximum: 1000 }, allow_blank: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_134712) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_153758) do
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
@@ -18,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_02_134712) do
     t.integer "quantidade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "departamento_id"
   end
 
 end
